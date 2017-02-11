9a4a56e2cd2f06ba3395b461dc00dda758265655
Utkarsh Mittal <utkarsh.mittal@icloud.com>
2017-02-11 15:08:33 +0530

    added listView in prescriptions fragment

--- MedExApp/app/src/main/java/com/hashinclude/medex/fragments/Prescriptions.java
+++ MedExApp/app/src/main/java/com/hashinclude/medex/fragments/Prescriptions.java
@@ -1,10 +1,16 @@
 package com.hashinclude.medex.fragments;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +19,19 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.hashinclude.medex.R;
+import com.hashinclude.medex.adapters.CardsAdapter;
+import com.hashinclude.medex.models.Cards;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pankaj on 11/2/17.
  */
 public class Prescriptions extends Fragment {
     View rootView;
+    private CardsAdapter adapter;
+    private List<Cards> CardsList;
 
     @Nullable
     @Override
@@ -26,6 +39,19 @@ public class Prescriptions extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_prescriptions, container, false);
 
         initCollapsingToolbar();
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+
+        CardsList = new ArrayList<>();
+        adapter = new CardsAdapter(getContext(), CardsList, "Tips");
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext(), 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new Prescriptions.GridSpacingItemDecoration(1, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        prepareCards();
+        
         try {
             Glide.with(getContext()).load(R.drawable.cover).into((ImageView) rootView.findViewById(R.id.backdrop));
         } catch (Exception e) {
@@ -37,6 +63,7 @@ public class Prescriptions extends Fragment {
     }
 
     private void init() {
+        
 
     }
 
@@ -76,5 +103,85 @@ public class Prescriptions extends Fragment {
                 }
             }
         });
+    }
+
+    private void prepareCards() {
+        int[] covers = new int[]{
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3,
+                R.drawable.image4,
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3,
+                R.drawable.image4,
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3
+        };
+
+        Cards a = new Cards("Prescription 1", covers[0], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+
+        a = new Cards("Prescription 2", covers[1], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+        a = new Cards("Prescription 3", covers[2], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+        a = new Cards("Prescription 4", covers[3], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+        a = new Cards("Prescription 5", covers[4], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+        a = new Cards("Prescription 6", covers[5], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+        a = new Cards("Prescription 7", covers[6], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et placerat neque, blandit pellentesque mauris");
+        CardsList.add(a);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
